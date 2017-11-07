34d495b68c7c3375f08cdf915de2183439edb5ea
Vinícius Negrão de Menezes <vinegrao95@hotmail.com>
2017-11-07 10:55:28 -0300

    Add files via upload

---
+++ db_pie.php
@@ -0,0 +1,57 @@
+<?php
+
+/* ****************************************************************** */
+//
+//  ***** ***** ***** ********* Easy Dash ********* ***** ***** *****
+//
+//  Description: A tool that stablish and communicate with Danfoss 
+//  equipaments (AK-SC255 / AK-SC355 / AK-SM88x), 
+//  using the controller web server API 
+//  (See XML_Interface 1_0 (Deprecated) - Danfoss Manual).
+//
+//  Author: F
+//
+//  Company: GreenYellow do Brasil.
+//
+/* ****************************************************************** */
+
+/* ****************************************************************** */
+//
+//  data base connection and query execution
+//
+/* ****************************************************************** */
+
+function dbUpdate($sql) {
+
+    // seting the database parameters...
+    $servername = "10.155.131.16";
+    $username = "easy";
+    $password = "easy";
+    $dbname = "EASY_HISTORIC";
+
+    // create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // check connection
+    if($conn->connect_error) {
+        logUpdate($dbLogFile, die("Connection failed: " . $conn->connect_error));
+    }
+
+    // cxecute query
+        $run = mysqli_query($conn, $sql);
+        if (!$run) {
+        printf("Error: %s\n", mysqli_error($conn));
+        exit();
+}
+        $return = mysqli_fetch_num_rows($run);
+        return $return;
+        //logUpdate($dbLogFile, "Record updated successfully: " . $sql);
+
+    // close connection
+    $conn->close();
+
+}
+
+/* ****************************************************************** */
+
+?>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php header('Access-Control-Allow-Origin: *');
+session_start();
+include ("db.php");
+$semana_atual = date('W'); //2017-11-01
+$semana1_query = $semana_atual - 1;
+$semana2_query = $semana_atual - 2;
+
+
+$dados_semana_atual = "select * from EASY_HISTORIC.STORE_STATUS WHERE SEMANA_NUMERO = $semana_atual";
+$dados_semana1 = "select ALL_IL, MANU_IL, ALL_FA, MANU_FA, ALL_AC, MANU_AC from STORE_STATUS WHERE SEMANA_NUMERO = $semana1_query";
+$dados_semana2 = "select ALL_IL, MANU_IL, ALL_FA, MANU_FA, ALL_AC, MANU_AC from STORE_STATUS WHERE SEMANA_NUMERO = $semana2_query";
+
+$atual = dbUpdate($dados_semana_atual);
+$semana1 = dbUpdate($dados_semana1);
+$semana2 = dbUpdate($dados_semana2);
+
+
+$dados = array(
+    "il_man_atual" => $atual['ALL_IL'],
+    "il_all_atual" => $atual['MANU_IL'],
+    "fa_man_atual" => $atual['MANU_FA'],
+    "fa_all_atual" => $atual['ALL_FA'],
+    "ac_man_atual" => $atual['MANU_AC'],
+    "ac_all_atual" => $atual['ALL_AC'],
+    //SEMANA -1
+    "il_all_1" => $semana1['ALL_IL'],
+    "il_man_1" => $semana1['MANU_IL'],
+    "fa_man_1" => $semana1['MANU_FA'],
+    "fa_all_1" => $semana1['ALL_FA'],
+    "ac_man_1" => $semana1['MANU_AC'],
+    "ac_all_1" => $semana1['ALL_AC'],
+    //SEMANA -2
+    "il_all_2" => $semana2['ALL_IL'],
+    "il_man_2" => $semana2['MANU_IL'],
+    "fa_man_2" => $semana2['MANU_FA'],
+    "fa_all_2" => $semana2['ALL_FA'],
+    "ac_man_2" => $semana2['MANU_AC'],
+    "ac_all_2" => $semana2['ALL_AC']
+);
+
+print_r(json_encode($dados));
+/*call status_lojas('2017-11-01', '0')*/
+?>
